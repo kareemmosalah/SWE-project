247ae2fb6d1168db42cbe88a1dcedfdd4da9921e
kk <mm>
2024-11-23 04:22:42 +0200

    Remove nested Git repository and add front-end folder

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
+def index(request):
+    return render(request, 'index.html')  # React app's entry point
+
+urlpatterns = [
+    # API routes go here, e.g., path('api/', include('api.urls'))
+    path('', index),  # Serve React app for root URL
+    re_path(r'^(?!api/).*$', index),  # Serve React app for all other routes except API
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
