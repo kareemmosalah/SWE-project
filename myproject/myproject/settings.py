1ad12de4b2663c4a872bd0a572e37fc8383ad659
kk <mm>
2024-12-22 07:47:00 +0200

    ci/cd

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -171,3 +171,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://tegyhagzwebapp.azurewebsites.net',
+]
